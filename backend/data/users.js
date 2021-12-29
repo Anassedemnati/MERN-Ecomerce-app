8839b7a3e071dd082bacf6d622473aed7c4fde3e
Anasse Demnati <demnati_anasse@emsi-edu.ma>
2021-12-29 12:41:54 +0100

    preparing sample data of

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs"
+const users =[
+    {
+        name:'Admin User',
+        email:'admin@example.com',
+        password:bcrypt.hashSync('123456',10),
+        isAdmin:true
+    },
+    {
+        name:'Anasse demnati',
+        email:'Anasse@gmail.com',
+        password:bcrypt.hashSync('123456',10),
+        
+    },
+    {
+        name:'Fathi Mahdi',
+        email:'Fathi@gmail.com',
+        password:bcrypt.hashSync('123456',10),
+       
+    },
+]
+export default users;
