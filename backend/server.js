a360d006ebbd9883f20cf938327f2060abc5da17
Anasse Demnati <demnati_anasse@emsi-edu.ma>
2021-12-30 21:21:26 +0100

    error handling to root

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'
 import colors from 'colors'
-import products from './data/products.js';
+import  {notFound,errorHandler} from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
 
 
 
@@ -15,21 +16,20 @@ connectDB()
 
 const app= express();
 
-
-
-
 app.get('/',(req,res)=>{
     res.send('api is running....');
 })
 
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
+app.use('/api/products',productRoutes)
 
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find(p=>p._id===req.params.id)
-    res.json(product)
-})
+app.use(notFound);
+
+//Error handling
+app.use(errorHandler);
+
+
+
+
 
 const PORT = process.env.PORT || 5000
 
