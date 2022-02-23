ece503b098161c008f427c20dfdb024530b59661
Anasse Demnati <demnati_anasse@emsi-edu.ma>
2022-02-23 12:18:30 +0100

    User Registration & Password Encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUsers,authUser,getUserProfile } from '../controllers/userController.js'
+import { getUsers,authUser,getUserProfile,registerUser } from '../controllers/userController.js'
 import { protect } from "../middleware/authMiddleware.js";
 
 
@@ -9,12 +9,15 @@ const router = express.Router();
 router.post('/login',authUser);
 
 
+
 router.route('/profile').get(protect,getUserProfile);
 
 
 
 
-router.route('/').get(getUsers)
+router.route('/').get(getUsers);
+
+router.route('/').post(registerUser);
 
 
 
